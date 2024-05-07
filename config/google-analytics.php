7af7fb213b7f2ef7e8fd4477519db686266dfa4d
vitorbizarra <vitorbizarracontato@gmail.com>
2024-05-06 22:56:44 -0300

    :construction: wip: Configuration file

--- config/google-analytics.php
+++ config/google-analytics.php
@@ -1,6 +1,18 @@
 <?php
 
-// config for VitorBizarra/GoogleAnalytics
 return [
+    /*
+     * The Google Analytics id, should be a code that looks something like "G-xxxx".
+     */
+    'id' => env('GOOGLE_ANALYTICS_ID', ''),
 
+    /*
+     * Enable or disable script rendering. Useful for local development.
+     */
+    'enabled' => env('GOOGLE_ANALYTICS_ENABLED', true),
+
+    /*
+     * Configures the Google Analytics script domain.
+     */
+    'domain' => env('GOOGLE_ANALYTICS_DOMAIN', 'www.googletagmanager.com'),
 ];
